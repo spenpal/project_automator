d4cfd2e11db7f26a308f995cf870f91cca63f8cf
spenpal2000 <sanjeev.penupala@gmail.com>
2020-12-15 16:23:14 -0600

    Updated bash unix path bug

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+reldir=`dirname $0`
+cd $reldir
+directory=`pwd`
 
 function create() {
     # shellcheck disable=SC2164
-    cd "$SCRIPTPATH"
+    cd "$directory"
     python main.py "$@"
 }
