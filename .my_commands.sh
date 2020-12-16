47bb2077e57ddcc92d83f3ab96aa76409a6c5096
spenpal2000 <sanjeev.penupala@gmail.com>
2020-12-16 01:21:35 -0600

    Suppresed python v. python3 warnings

--- .my_commands.sh
+++ .my_commands.sh
@@ -7,5 +7,5 @@ SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function create() {
     cd "$SCRIPTPATH"
-    python main.py "$@" 2> /dev/null || python3 main.py "$@"
+    python main.py "$@" 2>/dev/null || python3 main.py "$@" 2>/dev/null
 }
