94fdcfce0db2e0436974918ef27f88c1aa7af9d4
spenpal2000 <sanjeev.penupala@gmail.com>
2020-12-15 23:48:56 -0600

    Fixed bug with python vs. python3

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-reldir=`dirname $0`
-cd $reldir
-directory=`pwd`
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# reldir=`dirname $0`
+# cd $reldir
+# directory=`pwd`
 
 function create() {
-    # shellcheck disable=SC2164
-    cd "$directory"
-    python main.py "$@" || python3 main.py "$@"
+    cd "$SCRIPTPATH"
+    python main.py "$@" 2> /dev/null || python3 main.py "$@"
 }
