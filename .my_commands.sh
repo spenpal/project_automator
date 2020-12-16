e4a194612b280d506491be2cb476a83a191dc673
spenpal2000 <sanjeev.penupala@gmail.com>
2020-12-16 16:20:13 -0600

    Fixed UNIX issue with dirname

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# reldir=`dirname $0`
-# cd $reldir
-# directory=`pwd`
+reldir=`dirname $0`
+cd $reldir
+directory=`pwd`
 
 function create() {
-    cd "$SCRIPTPATH"
+    cd "$directory"
     python main.py "$@" 2>/dev/null || python3 main.py "$@" 2>/dev/null
 }
